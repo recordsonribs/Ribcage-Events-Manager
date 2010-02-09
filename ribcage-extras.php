6cd0abbd7e8801f28f8442508242d8e975b63c80
alex@recordsonribs.com <alex@recordsonribs.com>
2010-02-09 02:06:28 +0000

    Added the first file of Ribcage adaptions.

---
+++ ribcage-extras.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Additional functions to make Events Manager play nicely with Ribcage.
+ *
+ * @author Alex Andrews (alex@recordsonribs.com)
+ **/
+
+/**
+ * The following additions expand categories to allow them to be used more
+ * easily as 'artists' as on the Records On Ribs website.
+ **/
+
+/**
+ * Creates a slug from a category name.
+ *
+ * @author Alex Andrews (alex@recordsonribs.com)
+ */
+function dbem_categories_slug (){
+	
+}
+
+/**
+ * Converts a category slug into its numerical category ID for lookup.
+ *
+ * @return void
+ * @author Alex Andrews (alex@recordsonribs.com)
+ **/
+function dbem_category_slug_to_id ()
+{
+}
+
+
+?>
